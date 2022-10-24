12b6f510636f525b756fe30e5101ca872c06b138
Antonio Rodriguez Ruiz <antoniorodriguezruiz2002@gmail.com>
2022-10-24 12:36:01 +0200

    [doc]: Added info about uied_params

--- featureextraction/detection.py
+++ featureextraction/detection.py
@@ -100,6 +100,20 @@ def get_uied_gui_components_crops(input_imgs_path, image_names, img_index):
     resize_by_height = 800
     input_img_path = pjoin(input_imgs_path, image_names[img_index])
 
+    '''
+    ele:min-grad: gradient threshold to produce binary map         
+    ele:ffl-block: fill-flood threshold
+    ele:min-ele-area: minimum area for selected elements 
+    ele:merge-contained-ele: if True, merge elements contained in others
+    text:max-word-inline-gap: words with smaller distance than the gap are counted as a line
+    text:max-line-gap: lines with smaller distance than the gap are counted as a paragraph
+
+    Tips:
+    1. Larger *min-grad* produces fine-grained binary-map while prone to over-segment element to small pieces
+    2. Smaller *min-ele-area* leaves tiny elements while prone to produce noises
+    3. If not *merge-contained-ele*, the elements inside others will be recognized, while prone to produce noises
+    4. The *max-word-inline-gap* and *max-line-gap* should be dependent on the input image size and resolution
+    '''
     uied_params = {
         'min-grad': 3,
         'ffl-block': 5,
