845caf970468859f4c4ed896d7e5553c652c6943
Antonio <antoniommrr@gmail.com>
2023-03-04 19:53:19 +0000

    [feat]: extra tag to print object attributes as json

---
+++ apps/templatetags/extras.py
@@ -0,0 +1,17 @@
+import json
+
+from django import template
+from apps.featureextraction.models import FeatureExtractionTechnique, UIElementsDetection, UIElementsClassification, NoiseFiltering
+from apps.decisiondiscovery.models import ExtractTrainingDataset, DecisionTreeTraining
+
+register = template.Library()
+
+@register.filter
+def pretty_json(value):
+    # classname = value.__class__.__name__
+    # obj = classname.objects.get(value.id)
+    if value:
+        value = value.__dict__
+        value.pop("_state")
+        value.pop("id")
+    return value
