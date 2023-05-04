d8ac3803d4aefd09ce81fbaa5165b190e70f22b7
Antonio <antoniommrr@gmail.com>
2023-05-04 14:27:52 +0000

    [fix]: reporting pdd urls

--- apps/reporting/urls.py
+++ apps/reporting/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'reporting'
 
 urlpatterns = [
-    path('bpmn/list/', views.ReportListView.as_view(), name='report_list'),
-    path('bpmn/download/<int:report_id>', views.ReportListView.as_view(), name='report_download'),
-    path('bpmn/generate/<int:case_study_id>', views.ReportGenerateView.as_view(), name='report_generate'),
+    path('pdd/list/', views.ReportListView.as_view(), name='report_list'),
+    path('pdd/download/<int:report_id>', views.ReportListView.as_view(), name='report_download'),
+    path('pdd/generate/<int:case_study_id>', views.ReportGenerateView.as_view(), name='report_generate'),
 ]
