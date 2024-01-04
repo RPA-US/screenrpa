1292b64a07dd7179edcf3bb96e42c351d9d1b4a7
juagallop1 <juagallop1@alum.us.es>
2024-01-04 09:27:52 +0000

    refactor: deleted old comments

--- apps/analyzer/forms.py
+++ apps/analyzer/forms.py
@@ -25,12 +25,8 @@ class CaseStudyForm(forms.ModelForm):
             "title",
             "description",
             "exp_file",
-            # "exp_foldername",
-            # "exp_folder_complete_path",
             "scenarios_to_study",
             "special_colnames",
-            # "text_classname",
-            # "decision_point_activity",
             "gui_class_success_regex",
             "target_label"
         )
@@ -38,15 +34,9 @@ class CaseStudyForm(forms.ModelForm):
             "title": _("Title"),
             "description": _("Description"),
             "exp_file": _("Experiment file"),
-            # "exp_foldername": _("Experiment foldername"),
-            # "exp_folder_complete_path": _("Experiment folder complete path"),
             "scenarios_to_study": _("Scenarios to study"),
             "special_colnames": _("Special colnames"),
-            # "text_classname": _("Text classname"),
-            # "decision_point_activity": _("Decision point activity"),
             "gui_class_success_regex": _("GUI class success regex"),
-            # "ui_elements_classification_image_shape": _("UI elements classification image shape"),
-            # "ui_elements_classification_classes": _("UI elements classification classes"),
             "target_label": _("Target label")
         }
 
@@ -68,18 +58,6 @@ class CaseStudyForm(forms.ModelForm):
                     'accept': '.zip'
                     }
             ),
-            # "exp_foldername": forms.TextInput(
-            #     attrs={
-            #         "class": "form-control",
-            #         "placeholder": "exp_folder"
-            #         }
-            # ),
-            # "exp_folder_complete_path": forms.TextInput(
-            #     attrs={
-            #         "class": "form-control",
-            #         "placeholder": "/rim/resources/exp_folder"
-            #         }
-            # ),
             "scenarios_to_study": forms.TextInput(
                 attrs={"class": "form-control", "placeholder": "['scenario_1', 'scenario_2']"}
             ),
@@ -89,36 +67,12 @@ class CaseStudyForm(forms.ModelForm):
                     "placeholder": "special_colnames"
                     }
             ),
-            # "text_classname": forms.TextInput(
-            #     attrs={
-            #         "class": "form-control",
-            #         "placeholder": "text_classname"
-            #         }
-            # ),
-            # "decision_point_activity": forms.TextInput(
-            #     attrs={
-            #         "class": "form-control",
-            #         "placeholder": "decision_point_activity"
-            #         }
-            # ),
             "gui_class_success_regex": forms.TextInput(
                 attrs={
                     "class": "form-control",
                     "placeholder": "gui_class_success_regex"
                     }
             ),
-            # "ui_elements_classification_image_shape": forms.TextInput(
-            #     attrs={
-            #         "class": "custom-select",
-            #         "placeholder": "ui_elements_classification_image_shape"
-            #         }
-            # ),
-            # "ui_elements_classification_classes": forms.Textarea(
-            #     attrs={
-            #         "class": "form-control",
-            #         "placeholder": "ui_elements_classification_classes"
-            #         }
-            # ),
             "target_label": forms.TextInput(
                 attrs={
                     "class": "form-control",
@@ -130,31 +84,6 @@ class CaseStudyForm(forms.ModelForm):
                     "class": "custom-select"
                     }
             ),
-            # "monitoring": forms.Select(
-            #     attrs={
-            #         "class": "custom-select"
-            #         }
-            # ),
-            # "ui_elements_classification": forms.Select(
-            #     attrs={
-            #         "class": "custom-select"
-            #         }
-            # ),
-            # "feature_extraction_technique": forms.Select(
-            #     attrs={
-            #         "class": "custom-select"
-            #         }
-            # ),
-            # "extract_training_dataset": forms.Select(
-            #     attrs={
-            #         "class": "custom-select"
-            #         }
-            # ),
-            # "decision_tree_training": forms.Select(
-            #     attrs={
-            #         "class": "custom-select"
-            #         }
-            # )
         }
 
     def clean_title(self):
@@ -167,4 +96,3 @@ class CaseStudyForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(CaseStudyForm, self).__init__(*args, **kwargs)
-
