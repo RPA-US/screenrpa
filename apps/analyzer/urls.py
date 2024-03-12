31cf407984f5680f5087891b34f568a5d8a3d098
Alejandro Ingles <alejmart@gmail.com>
2024-03-12 12:05:33 +0000

    [feat]: disabled logProcessing-decisionTreeTraining buttom

--- apps/analyzer/urls.py
+++ apps/analyzer/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
     path('execution/list/', views.ExecutionListView.as_view(), name='execution_list'),
     path('execution/delete/', views.deleteExecution, name='execution_delete'),
     path('execution/detail/<int:execution_id>/', views.ExecutionDetailView.as_view(), name='execution_detail'),
+
+    # Phases Executions results
+    #path('execution/phase_result/<int:execution_id>/', views.PhaseExecutionResultDetailView.as_view(), name='phase_execution_results'),
 ]
 
