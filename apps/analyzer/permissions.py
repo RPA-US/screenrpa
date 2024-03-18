89e829ab7c6ab82f789eba10c25cccc966d8df9a
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-03-18 11:01:28 +0000

    fix: Retrieve execution results trhough privatefiles

--- apps/analyzer/permissions.py
+++ apps/analyzer/permissions.py
@@ -4,5 +4,6 @@ Possible functions for the ``PRIVATE_STORAGE_AUTH_FUNCTION`` setting.
 from .models import CaseStudy
 
 def allow_staff(private_file):
-    name = private_file.relative_name[:len(private_file.relative_name)-1]
-    return CaseStudy.objects.filter(user=private_file.request.user, exp_file=name).exists()
+    resource_name = private_file.relative_name[:len(private_file.relative_name)]
+    foldername = resource_name.split('/executions')[0].split("unzipped/")[1]
+    return CaseStudy.objects.filter(user=private_file.request.user, exp_foldername=foldername).exists()
