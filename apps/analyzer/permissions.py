0cedf458cd2cde352998f606014f5d5a8124d71a
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-03-20 08:29:15 +0000

    fix: Not retrieving image after path origin change

--- apps/analyzer/permissions.py
+++ apps/analyzer/permissions.py
@@ -5,5 +5,5 @@ from .models import CaseStudy
 
 def allow_staff(private_file):
     resource_name = private_file.relative_name[:len(private_file.relative_name)]
-    foldername = resource_name.split('/executions')[0].split("unzipped/")[1]
+    foldername = resource_name.split("unzipped/")[1].split('/')[0]
     return CaseStudy.objects.filter(user=private_file.request.user, exp_foldername=foldername).exists()
