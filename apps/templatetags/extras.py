2c0eb777a7d192486383843b1b16db445693ab73
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-03-21 10:39:14 +0000

    fix: None text values in som

--- apps/templatetags/extras.py
+++ apps/templatetags/extras.py
@@ -13,3 +13,17 @@ def divide(value, arg):
         return round(int(value) / int(arg),2)
     except (ValueError, ZeroDivisionError):
         return None
+
+@register.filter
+def handle_none(value):
+    def replace_none(obj):
+        if isinstance(obj, dict):
+            return {k: replace_none(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [replace_none(elem) for elem in obj]
+        elif obj is None:
+            return ""
+        else:
+            return obj
+
+    return json.dumps(replace_none(value))
