4f7d9c7f2e1d7b33f55682b3ac7dd9e9e7f692e0
Antonio <antoniommrr@gmail.com>
2024-05-27 12:18:44 +0200

    feat: automatic separator recognition in ui logs

--- core/utils.py
+++ core/utils.py
@@ -2,8 +2,27 @@ import pandas as pd
 import json
 from .settings import MODELS_CLASSES_FILEPATH
 
+def detect_separator(file_path):
+    # Leer las primeras líneas del archivo
+    with open(file_path, 'r') as file:
+        first_line = file.readline()
+    
+    # Determinar el separador basado en la primera línea
+    if ',' in first_line and ';' in first_line:
+        raise ValueError("Ambos separadores ',' y ';' están presentes en la primera línea. No se puede determinar el separador automáticamente.")
+    elif ',' in first_line:
+        separator = ','
+    elif ';' in first_line:
+        separator = ';'
+    else:
+        raise ValueError("No se pudo determinar el separador. Asegúrese de que el archivo tenga un separador ',' o ';'.")
+    
+    return separator
+
 def read_ui_log_as_dataframe(log_path):
-  return pd.read_csv(log_path, sep=",")#, index_col=0)
+  # Reconoce automaticamente si los separadores son coma o punto y coma, en el dataframe
+  separator = detect_separator(log_path)
+  return pd.read_csv(log_path, sep=separator)#, index_col=0)
 
 def get_model_classes(execution):
     f = open(MODELS_CLASSES_FILEPATH)
