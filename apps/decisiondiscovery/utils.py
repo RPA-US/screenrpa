bc45f28c1bac097747e61f565e2443fba828e0d1
Alejandro Ingles <alejmart@gmail.com>
2024-06-10 08:19:47 +0000

    [fix]: new regex to capture one_hot_ expession in decision trees

--- apps/decisiondiscovery/utils.py
+++ apps/decisiondiscovery/utils.py
@@ -264,6 +264,11 @@ def read_feature_column_name(column_name):
         aux1 = 0
         centroid = None
         aux2 = 0
+    elif not "__" in column_name and not contains_centroid:
+        pattern = r"([a-zA-Z_]+)__([a-zA-Z]+)_(\d+_\d+)"
+        suffix = None
+        aux1 = 0
+        aux2 = 1
     else:
         pattern = r"([a-zA-Z]+_[a-zA-Z]+)_(\d+\.\d+-\d+\.\d+)_(\d*_?[a-zA-Z])"
         suffix = None
