bbbcb8920bf2fdc23ec9a84426d31630a0719230
Alejandro Ingles <alejmart@gmail.com>
2024-06-17 15:20:16 +0000

    [fix]: error when executing feature extraction

--- apps/featureextraction/UIFEs/single_feature_extraction_techniques.py
+++ apps/featureextraction/UIFEs/single_feature_extraction_techniques.py
@@ -228,8 +228,8 @@ def aux_iterate_compos(ui_log_path, path_scenario, execution, fe, centroid_colum
 # ========================================================================================================
                 elif centroid_columnname_type == "centroid_class":
                     centroid = compos_list[j]["centroid"]
-                    activity = log.at[i, activity_colname]
-                    column_name = f"{id}_{centroid[0]}-{centroid[1]}_{activity}"
+#                    activity = log.at[i, activity_colname]
+                    column_name = f"{id}_{centroid[0]}-{centroid[1]}"
                     
                     if column_name in info_to_join:
                         if not len(info_to_join[column_name]) == i:
