3ea299a4fc756e63044e7fea2bba0e47f5cde37d
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-06-19 09:30:17 +0000

    fix: get notifications and mark all as read

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -7,5 +7,6 @@ app_name = 'notification'
 urlpatterns = [
     path('', views.get_notifications, name='notification_list'),
     path('mark_as_read/', views.mark_as_read, name='mark_as_read'),
+    path('mark_all_as_read/', views.mark_all_as_read, name='mark_as_read'),
     path('delete/', views.delete_notification, name='delete'),
 ]
--- apps/notification/views.py
+++ apps/notification/views.py
@@ -1,7 +1,5 @@
 import json
-from django.shortcuts import get_object_or_404, render
-from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Notification
@@ -11,9 +9,19 @@ from .models import Notification
 @login_required(login_url='/login/')
 def get_notifications(request):
     queryset = Notification.objects.filter(user=request.user).order_by('-created_at')
+    data = []
+    if queryset.exists():
+        data = list(map(lambda x: {
+            'id': x.id,
+            'short': x.short,
+            'message': x.message,
+            'read': x.read,
+            'href': x.href,
+            'created_at': x.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        }, queryset))
     res = {
         'unread': Notification.objects.filter(user=request.user, read=False).count(),
-        'notifications': queryset.get() if queryset.exists() else []
+        'notifications': data
     }
     return HttpResponse(json.dumps(res), content_type='application/json')
 
@@ -25,6 +33,11 @@ def mark_as_read(request):
         return HttpResponse(status=403)
     notification.read = True
     notification.save()
+    return HttpResponse(json.dumps({'success': True}), content_type='application/json')
+    
+@login_required(login_url='/login/')
+def mark_all_as_read(request):
+    Notification.objects.filter(user=request.user, read=False).update(read=True)
     return HttpResponse(json.dumps({'success': True}), content_type='application/json')
 
 @login_required(login_url='/login/')
@@ -39,4 +52,4 @@ def delete_notification(request):
 ## Utils for other modules
 
 def create_notification(user, short, message, href):
-    Notification.objects.create(user=user, short=short, message=message, href=href)
+    Notification.objects.create(user=user, short=short, message=message, href=href).save()
