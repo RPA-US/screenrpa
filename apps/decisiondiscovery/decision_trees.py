e1306561f5abff7dd4f6249236c6584341d567d2
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-10-14 12:37:23 +0200

    fix: Handle nested dps

--- apps/decisiondiscovery/decision_trees.py
+++ apps/decisiondiscovery/decision_trees.py
@@ -15,6 +15,7 @@ from sklearn.tree import DecisionTreeClassifier, export_graphviz, export_text
 from .utils import def_preprocessor, best_model_grid_search, cross_validation, extract_tree_rules, prev_preprocessor
 from apps.chefboost import Chefboost as chef
 from apps.notification.views import create_notification
+from apps.processdiscovery.utils import Process, Rule
 from core.settings import PLOT_DECISION_TREES, SEVERAL_ITERATIONS
 import pickle
 
@@ -165,23 +166,27 @@ def plot_decision_tree(path: str,
 
     return image
 
-def update_json_with_rules(json_data, rules_class):
+def update_json_with_rules(process: Process, rules_class):
+    rules_class = {str(k): v for k, v in rules_class.items()}
     # Recorrer los puntos de decisión
-    for decision_point in json_data["decision_points"]:
+    for decision_point in process.get_non_empty_dp_flattened():
         # Recorrer las ramas del punto de decisión
-        for branch in decision_point["branches"]:
-            label = branch["label"]
+        for branch in decision_point.branches:
+            label = branch.label
             
             # Si hay reglas para esta rama en rules_class_str_keys, actualizar el JSON
-            if label in rules_class:
-                if label in decision_point["rules"]:
+            if str(label) in rules_class.keys():
+                contained = list(map(lambda r: str(r.target) == str(label), decision_point.rules))
+                if any(contained):
                     # Añadir nuevas reglas a la lista existente
-                    decision_point["rules"][label].extend(rules_class[label])
+                    rule = decision_point.rules[contained.index(True)]
+                    rule.condition.extend(rules_class[str(label)])
                 else:
                     # Crear una nueva lista de reglas si no existe
-                    decision_point["rules"][label] = rules_class[label]
+                    rule = Rule(rules_class[str(label)], str(label))
+                    decision_point.rules.append(rule)
     
-    return json_data
+    return process
 
 def sklearn_decision_tree(df,prevact, param_path, special_colnames, configuration, balance_weights, one_hot_columns, target_label, k_fold_cross_validation, execution):
     times = {}
@@ -294,11 +299,12 @@ def sklearn_decision_tree(df,prevact, param_path, special_colnames, configuratio
     if rules_class is not None:
         with open(traceability_path, 'r') as f:
             json_data = json.load(f)
+            process = Process.from_json(json_data)
         
-        updated_json = update_json_with_rules(json_data, rules_class)
+        process = update_json_with_rules(process, rules_class)
         
         with open(traceability_path, 'w') as f:
-            json.dump(updated_json, f, indent=2)
+            json.dump(process.to_json(), f, indent=2)
 
 
     if PLOT_DECISION_TREES:
